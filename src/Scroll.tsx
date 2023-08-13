2117d5b684c17efbd5949b15ed85237b879c14da
IbRuslan <ribragimov2003@gmail.com>
2023-08-13 22:46:50 +0500

    scroll on wheel

--- src/Scroll.tsx
+++ src/Scroll.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef, WheelEvent} from 'react';
 
 type ScrollTypeProps = {
     children: React.ReactNode
@@ -7,8 +7,26 @@ type ScrollTypeProps = {
 
 export const Scroll: React.FC<ScrollTypeProps> = ({children, style}) => {
 
+    const ref = useRef<HTMLDivElement | null>(null)
+
+    useEffect(()=> {
+        const el = ref.current
+        if(el) {
+            const onWheel = (e: WheelEvent<HTMLDivElement>) => {
+                e.preventDefault()
+                el.scrollTo({
+                    left: el.scrollLeft + e.deltaY * 4,
+                    behavior: 'smooth'
+                })
+            }
+            el.addEventListener('wheel', onWheel as unknown as EventListener)
+
+            return () => el.removeEventListener('wheel', onWheel as unknown as EventListener)
+        }
+    }, [])
+
     return (
-        <div className={style}>
+        <div ref={ref} className={style}>
             {
                 React.Children.map(children, child => React.Children.only(child))
             }
